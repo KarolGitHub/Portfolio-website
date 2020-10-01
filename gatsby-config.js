f7006986a654a5ea46f19c7df5f3bfbfa0fae747
Karol <karolgardyjas@gmail.com>
2020-10-01 15:10:48 +0200

    add netlify CMS config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,16 @@ module.exports = {
     description:
       'Blog made with Gatsby, Typescript, Javascript, React hooks, emotion, deployed using Netlify',
     author: 'Karol Gardyjas',
+    social: [
+      {
+        name: 'github',
+        url: 'https://github.com/KarolGitHub/Gatsby-blog',
+      },
+    ],
   },
-  plugins: [`gatsby-plugin-emotion`],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-react-helmet',
+  ],
 };
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -37,97 +37,94 @@ export const lightTheme = {
   },
 };
 
-export const GlobalStyles = (props) => {
-  console.log(props);
-  return (
-    <Global
-      {...props}
-      styles={css`
-        :root {
-          --site-max-width: 1200px;
+export const GlobalStyles = (props) => (
+  <Global
+    {...props}
+    styles={css`
+      :root {
+        --site-max-width: 1200px;
+      }
+      * {
+        box-sizing: border-box;
+      }
+
+      html,
+      body {
+        height: 100%;
+      }
+
+      body {
+        background: ${props.theme.colors.background};
+        color: ${props.theme.colors.textSecondary};
+        font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+          'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+          'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-weight: 300;
+        font-style: normal;
+        margin: 0;
+        transition: all 0.5s ease;
+        @media (min-width: 600px) {
+          padding: 0 20px 80px 20px;
         }
-        * {
-          box-sizing: border-box;
+      }
+
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+          monospace;
+      }
+
+      main {
+        margin: 0 auto;
+        max-width: 960px;
+      }
+
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        color: ${props.theme.colors.textMain};
+        font-family: 'Muli Black', Arial, Helvetica, sans-serif;
+        transition: all 0.5s ease;
+        letter-spacing: 1px;
+      }
+
+      p {
+        font-size: 0.9rem;
+        line-height: 1.2;
+
+        @media (min-width: 600px) {
+          font-size: 1.1rem;
+          line-height: 1.5;
         }
+      }
 
-        html,
-        body {
-          height: 100%;
+      h2 {
+        font-size: 1rem;
+        letter-spacing: 1.8px;
+        text-transform: uppercase;
+
+        @media (min-width: 600px) {
+          font-size: 1.5rem;
         }
+      }
 
-        body {
-          background: ${props.theme.colors.background};
-          color: ${props.theme.colors.textSecondary};
-          font-family: 'Open Sans', -apple-system, BlinkMacSystemFont,
-            'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
-            'Droid Sans', 'Helvetica Neue', sans-serif;
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-          font-weight: 300;
-          font-style: normal;
-          margin: 0;
-          transition: all 0.5s ease;
-          @media (min-width: 600px) {
-            padding: 0 20px 80px 20px;
-          }
+      h3 {
+        font-size: 0.8rem;
+        letter-spacing: 1.5px;
+        margin: 30px 0 -15px 0;
+
+        @media (min-width: 600px) {
+          font-size: 1.2rem;
         }
+      }
 
-        code {
-          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-            monospace;
-        }
-
-        main {
-          margin: 0 auto;
-          max-width: 960px;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          color: ${props.theme.colors.textMain};
-          font-family: 'Muli Black', Arial, Helvetica, sans-serif;
-          transition: all 0.5s ease;
-          letter-spacing: 1px;
-        }
-
-        p {
-          font-size: 0.9rem;
-          line-height: 1.2;
-
-          @media (min-width: 600px) {
-            font-size: 1.1rem;
-            line-height: 1.5;
-          }
-        }
-
-        h2 {
-          font-size: 1rem;
-          letter-spacing: 1.8px;
-          text-transform: uppercase;
-
-          @media (min-width: 600px) {
-            font-size: 1.5rem;
-          }
-        }
-
-        h3 {
-          font-size: 0.8rem;
-          letter-spacing: 1.5px;
-          margin: 30px 0 -15px 0;
-
-          @media (min-width: 600px) {
-            font-size: 1.2rem;
-          }
-        }
-
-        a {
-          text-decoration: none;
-        }
-      `}
-    />
-  );
-};
+      a {
+        text-decoration: none;
+      }
+    `}
+  />
+);
